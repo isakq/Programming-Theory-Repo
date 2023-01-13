1abb1bc640b0993617126ac77381724ee689aeca
isakq <121938291+isakq@users.noreply.github.com>
2023-01-13 14:37:42 +0100

    Update
    UI created

---
+++ Assets/Scripts/Robot.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Robot : MonoBehaviour
+{
+    public int health = 100;
+    public int baseDamage = 10;
+    public int dodgeCapability = 10;
+    public int armor = 20;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void Attack()
+    {
+
+    }
+
+    void Dodge()
+    {
+
+    }
+}
